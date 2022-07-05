746a0c74543bc5e72ea12ce6b47001515762e788
roshenmaghhan <roshenmaghhan@gmail.com>
2022-07-06 01:03:12 +0800

    Added allowed hosts with delimeter

--- aksara/settings.py
+++ aksara/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOST")
+ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOST").split(",")
 
 # Application definition
 
