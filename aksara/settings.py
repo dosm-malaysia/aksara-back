39610e2a444d44d6be19598895a027ef4f2456b9
roshenmaghhan <roshenmaghhan@gmail.com>
2022-07-06 01:33:29 +0800

    Added static roots

--- aksara/settings.py
+++ aksara/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
