aea7dcb40a960e74ddcddde4cdcf8677b0e20367
roshenmaghhan <roshenmaghhan@gmail.com>
2022-07-05 22:59:05 +0800

    Secret key dependencies added

--- aksara/settings.py
+++ aksara/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 from decouple import config
 
