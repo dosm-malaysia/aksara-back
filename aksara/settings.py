7407464165d3ed9d7c0e9b71e335592d50c1d53c
roshenmaghhan <roshenmaghhan@gmail.com>
2022-07-06 00:17:33 +0800

    Installed more requirements

--- aksara/settings.py
+++ aksara/settings.py
@@ -13,7 +13,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from django.core.management.utils import get_random_secret_key
 from pathlib import Path
-from decouple import config
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,13 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
