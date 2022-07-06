ea3e7e6d856fb7edd03b690ec2fdfa8fb3f13e6e
roshenmaghhan <roshenmaghhan@gmail.com>
2022-07-06 12:23:07 +0800

    Set debug operator

--- aksara/settings.py
+++ aksara/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = env("DEBUG") == "True"
 
 ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOST").split(",")
 
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
