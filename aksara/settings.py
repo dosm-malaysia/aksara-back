5720101ad256deff43e184d18c5d9612d9a0ec93
roshenmaghhan <roshenmaghhan@gmail.com>
2022-09-29 18:23:36 +0800

    Added django_extensions

--- aksara/settings.py
+++ aksara/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'aksara',
     'corsheaders',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
