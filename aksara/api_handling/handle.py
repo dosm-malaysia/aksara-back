b448e65eb5852cdca547d20238afae0990e046af
roshenmaghhan <roshenmaghhan@gmail.com>
2022-12-28 17:17:44 +0800

    Msia data hardcode for kawasanku_admin API

--- aksara/api_handling/handle.py
+++ aksara/api_handling/handle.py
@@ -12,5 +12,12 @@ def dashboard_additional_handling(params, res) :
         catalog_count = CatalogJson.objects.all().count()  
         res['total_catalog'] = catalog_count
         return res
+    if dashboard == 'kawasanku_admin' : 
+        if params['area-type'][0] == 'country' : 
+            temp = res['jitter_chart']['data']['state']
+            res['jitter_chart']['data'] = temp
+            return res
+
+        return res    
     else : # Default if no additions to make
         return res
