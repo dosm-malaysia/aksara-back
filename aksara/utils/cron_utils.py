29b1884a0fc646a4b2635da2c20bb5e97e263863
roshenmaghhan <roshenmaghhan@gmail.com>
2023-01-03 16:43:32 +0800

    git pre-commit hook added

--- aksara/utils/cron_utils.py
+++ aksara/utils/cron_utils.py
@@ -13,186 +13,218 @@ import requests
 import zipfile
 import json
 
-'''
+"""
 Creates a directory
-'''
-def create_directory(dir_name) :
-    try: 
+"""
+
+
+def create_directory(dir_name):
+    try:
         os.mkdir(os.path.join(os.getcwd(), dir_name))
-    except OSError as error: 
+    except OSError as error:
         print("Directory already exists, no need to create")
 
-'''
+
+"""
 Fetches entire content from a git repo
-'''
-def fetch_from_git(zip_name, git_url, git_token) :
+"""
+
+
+def fetch_from_git(zip_name, git_url, git_token):
     file_name = os.path.join(os.getcwd(), zip_name)
     headers = {
-        'Authorization': f'token {git_token}',
-        'Accept': 'application/vnd.github.v3.raw'
+        "Authorization": f"token {git_token}",
+        "Accept": "application/vnd.github.v3.raw",
     }
 
     res = {}
-    res['file_name'] = file_name
-    res['data'] = requests.get(git_url, headers=headers)
-    res['resp_code'] = res['data'].status_code
+    res["file_name"] = file_name
+    res["data"] = requests.get(git_url, headers=headers)
+    res["resp_code"] = res["data"].status_code
     return res
 
-'''
+
+"""
 Writes content as binary
-'''
-def write_as_binary(file_name, data) :
-    try : 
-        with open(file_name, 'wb') as f:
+"""
+
+
+def write_as_binary(file_name, data):
+    try:
+        with open(file_name, "wb") as f:
             f.write(data.content)
-    except : 
+    except:
         triggers.send_telegram("!! FILE ISSUES WRITING TO BINARY !!")
 
-'''
+
+"""
 Extracts zip file into desired directory
-'''
-def extract_zip(file_name, dir_name) :
-    try : 
-        with zipfile.ZipFile(file_name, 'r') as zip_ref:
+"""
+
+
+def extract_zip(file_name, dir_name):
+    try:
+        with zipfile.ZipFile(file_name, "r") as zip_ref:
             zip_ref.extractall(os.path.join(os.getcwd(), dir_name))
-    except : 
+    except:
         triggers.send_telegram("!! ZIP FILE EXTRACTION ISSUE !!")
 
-'''
+
+"""
 Performs data operations,
 such as update or rebuild
-'''
-def data_operation(operation, op_method) :
-    dir_name = 'AKSARA_SRC'
-    zip_name = 'repo.zip'
-    git_url = 'https://github.com/dosm-malaysia/aksara-data/archive/main.zip'
-    git_token = os.getenv('GITHUB_TOKEN', '-')
+"""
+
+
+def data_operation(operation, op_method):
+    dir_name = "AKSARA_SRC"
+    zip_name = "repo.zip"
+    git_url = "https://github.com/dosm-malaysia/aksara-data/archive/main.zip"
+    git_token = os.getenv("GITHUB_TOKEN", "-")
 
     triggers.send_telegram("--- PERFORMING " + op_method + " " + operation + " ---")
 
     create_directory(dir_name)
     res = fetch_from_git(zip_name, git_url, git_token)
-    if 'resp_code' in res and res['resp_code'] == 200 : 
-        write_as_binary(res['file_name'], res['data'])
-        extract_zip(res['file_name'], dir_name)
+    if "resp_code" in res and res["resp_code"] == 200:
+        write_as_binary(res["file_name"], res["data"])
+        extract_zip(res["file_name"], dir_name)
         data_utils.rebuild_dashboard_meta(operation, op_method)
         data_utils.rebuild_dashboard_charts(operation, op_method)
-    else : 
+    else:
         triggers.send_telegram("FAILED TO GET SOURCE DATA")
 
-def get_latest_info_git(type, commit_id) : 
+
+def get_latest_info_git(type, commit_id):
     url = "https://api.github.com/repos/dosm-malaysia/aksara-data/commits/main"
-    headers_accept = 'application/vnd.github.VERSION.sha'
+    headers_accept = "application/vnd.github.VERSION.sha"
 
-    git_token = os.getenv('GITHUB_TOKEN', '-')
+    git_token = os.getenv("GITHUB_TOKEN", "-")
 
-    if type == 'COMMIT' : 
-        url = url.replace('main', '')
+    if type == "COMMIT":
+        url = url.replace("main", "")
         url += commit_id
-        headers_accept = 'application/vnd.github+json'
+        headers_accept = "application/vnd.github+json"
 
-    res = requests.get(url, headers={'Authorization': f'token {git_token}','Accept': headers_accept})
+    res = requests.get(
+        url, headers={"Authorization": f"token {git_token}", "Accept": headers_accept}
+    )
 
-    if res.status_code == 200 : 
-        return str(res.content, 'UTF-8')
-    else : 
+    if res.status_code == 200:
+        return str(res.content, "UTF-8")
+    else:
         triggers.send_telegram("!!! FAILED TO GET GITHUB " + type + " !!!")
 
-def selective_update() :
-    dir_name = 'AKSARA_SRC'
-    zip_name = 'repo.zip'
-    git_url = 'https://github.com/dosm-malaysia/aksara-data/archive/main.zip'
-    git_token = os.getenv('GITHUB_TOKEN', '-')
+
+def selective_update():
+    dir_name = "AKSARA_SRC"
+    zip_name = "repo.zip"
+    git_url = "https://github.com/dosm-malaysia/aksara-data/archive/main.zip"
+    git_token = os.getenv("GITHUB_TOKEN", "-")
 
     triggers.send_telegram("--- PERFORMING SELECTIVE UPDATE ---")
 
     create_directory(dir_name)
     res = fetch_from_git(zip_name, git_url, git_token)
-    if 'resp_code' in res and res['resp_code'] == 200 : 
-        write_as_binary(res['file_name'], res['data'])
-        extract_zip(res['file_name'], dir_name)
-        
-        latest_sha = get_latest_info_git('SHA', '')
-        data = json.loads(get_latest_info_git('COMMIT', latest_sha))
-        changed_files = [ f['filename'] for f in data['files']]  
-        filtered_changes  = filter_changed_files(changed_files)
-        
-        if filtered_changes['dashboards_deleted'] : 
-            remove_files = [ x.replace(".json", "") for x in filtered_changes['dashboards_deleted']]
-            if remove_files : 
+    if "resp_code" in res and res["resp_code"] == 200:
+        write_as_binary(res["file_name"], res["data"])
+        extract_zip(res["file_name"], dir_name)
+
+        latest_sha = get_latest_info_git("SHA", "")
+        data = json.loads(get_latest_info_git("COMMIT", latest_sha))
+        changed_files = [f["filename"] for f in data["files"]]
+        filtered_changes = filter_changed_files(changed_files)
+
+        if filtered_changes["dashboards_deleted"]:
+            remove_files = [
+                x.replace(".json", "") for x in filtered_changes["dashboards_deleted"]
+            ]
+            if remove_files:
                 MetaJson.objects.filter(file_src__in=remove_files).delete()
 
-        if filtered_changes['catalog_deleted'] :
-            remove_files = [ x.replace(".json", "") for x in filtered_changes['catalog_deleted']]
-            if remove_files : 
+        if filtered_changes["catalog_deleted"]:
+            remove_files = [
+                x.replace(".json", "") for x in filtered_changes["catalog_deleted"]
+            ]
+            if remove_files:
                 CatalogJson.objects.filter(file_src__in=remove_files).delete()
 
-
-        if filtered_changes['dashboards'] : 
-            fin_files = [ x.replace(".json", "") for x in filtered_changes['dashboards']]
+        if filtered_changes["dashboards"]:
+            fin_files = [x.replace(".json", "") for x in filtered_changes["dashboards"]]
             file_list = ",".join(fin_files)
 
             triggers.send_telegram("Updating : " + file_list)
-            
+
             operation = "UPDATE " + file_list
             data_utils.rebuild_dashboard_meta(operation, "AUTO")
-            validate_info = data_utils.rebuild_dashboard_charts(operation, "AUTO")            
-            
-            dashboards_validate = validate_info['dashboard_list']
-            failed_dashboards = validate_info['failed_dashboards']            
-            
-            # Validate each dashboard
-            for dbd in dashboards_validate : 
-                if dbd not in failed_dashboards : 
-                    revalidate_frontend(dbd)
-                else : 
-                    triggers.send_telegram("Validation for " + dbd + " : " + " not sent.")
+            validate_info = data_utils.rebuild_dashboard_charts(operation, "AUTO")
 
-        if filtered_changes['catalog'] : 
-            fin_files = [ x.replace(".json", "") for x in filtered_changes['catalog']]
+            dashboards_validate = validate_info["dashboard_list"]
+            failed_dashboards = validate_info["failed_dashboards"]
+
+            # Validate each dashboard
+            for dbd in dashboards_validate:
+                if dbd not in failed_dashboards:
+                    revalidate_frontend(dbd)
+                else:
+                    triggers.send_telegram(
+                        "Validation for " + dbd + " : " + " not sent."
+                    )
+
+        if filtered_changes["catalog"]:
+            fin_files = [x.replace(".json", "") for x in filtered_changes["catalog"]]
             file_list = ",".join(fin_files)
-            operation = "UPDATE " + file_list  
+            operation = "UPDATE " + file_list
             catalog_builder.catalog_update(operation, "AUTO")
 
-    else :
+    else:
         triggers.send_telegram("FAILED TO GET SOURCE DATA")
 
 
-'''
+"""
 Filters the changed files for dashboards and catalog data
-'''
-def filter_changed_files(file_list) : 
-    changed_files = {'dashboards' : [], 'catalog' : [], 'dashboards_deleted' : [], 'catalog_deleted' : []}
+"""
 
-    for f in file_list :
+
+def filter_changed_files(file_list):
+    changed_files = {
+        "dashboards": [],
+        "catalog": [],
+        "dashboards_deleted": [],
+        "catalog_deleted": [],
+    }
+
+    for f in file_list:
         f_path = "AKSARA_SRC/aksara-data-main/" + f
         f_info = f.split("/")
-        if len(f_info) > 1 and f_info[0] in changed_files : 
-            if os.path.exists(f_path) : 
-                changed_files[ f_info[0] ].append(f_info[1])
-            else : 
-                changed_files[ f_info[0] + '_deleted'].append(f_info[1])
+        if len(f_info) > 1 and f_info[0] in changed_files:
+            if os.path.exists(f_path):
+                changed_files[f_info[0]].append(f_info[1])
+            else:
+                changed_files[f_info[0] + "_deleted"].append(f_info[1])
 
     return changed_files
 
 
-'''
+"""
 Revalidate Frontend
-'''
-def revalidate_frontend(dashboard) :
-    if dashboard not in common.FRONTEND_ENDPOINTS :
+"""
+
+
+def revalidate_frontend(dashboard):
+    if dashboard not in common.FRONTEND_ENDPOINTS:
         return -1
 
     endpoint = common.FRONTEND_ENDPOINTS[dashboard]
-    url = os.getenv('FRONTEND_URL', '-')
-    fe_auth = os.getenv('FRONTEND_REBUILD_AUTH', '-')
-    
-    headers = {'Authorization' : fe_auth}
-    body = {'route' : endpoint}
+    url = os.getenv("FRONTEND_URL", "-")
+    fe_auth = os.getenv("FRONTEND_REBUILD_AUTH", "-")
+
+    headers = {"Authorization": fe_auth}
+    body = {"route": endpoint}
 
     response = requests.post(url, headers=headers, data=body)
 
-    if response.status_code == 200 : 
+    if response.status_code == 200:
         triggers.send_telegram(dashboard + " page, successfully validated.")
-    else : 
+    else:
         triggers.send_telegram(dashboard + " page, failed to validated.")
--- aksara/views.py
+++ aksara/views.py
@@ -22,22 +22,23 @@ import os
 import environ
 
 
-
 env = environ.Env()
 environ.Env.read_env()
 
-'''
+"""
 TODO :
 1. Make sure filters for data variable includes English and BM
-'''
+"""
 
-class UPDATE(APIView) : 
+
+class UPDATE(APIView):
     def post(self, request, format=None):
         # if is_valid_request(request, os.getenv("WORKFLOW_TOKEN")) :
         thread = Thread(target=cron_utils.selective_update)
         thread.start()
         return Response(status=status.HTTP_200_OK)
         # return JsonResponse({'status':401,'message':"unauthorized"}, status=401)
+
 
 class DASHBOARD(APIView):
     def get(self, request, format=None):
@@ -54,6 +55,7 @@ class DASHBOARD(APIView):
         else:
             return JsonResponse({}, safe=False)
 
+
 class DATA_VARIABLE(APIView):
     def get(self, request, format=None):
         param_list = dict(request.GET)
@@ -65,231 +67,275 @@ class DATA_VARIABLE(APIView):
         else:
             return JsonResponse({}, safe=False)
 
-class DATA_CATALOG(APIView) :
+
+class DATA_CATALOG(APIView):
     def get(self, request, format=None):
         param_list = dict(request.GET)
         filters = get_filters_applied(param_list)
-        info = ''
-        if len(filters) > 0 : 
-            info = CatalogJson.objects.filter(filters).values('id', 'catalog_name', 'catalog_category', 'catalog_category_name', 'catalog_subcategory_name')                            
-        else : 
-            info = CatalogJson.objects.all().values('id', 'catalog_name', 'catalog_category', 'catalog_category_name', 'catalog_subcategory_name')
-        
+        info = ""
+        if len(filters) > 0:
+            info = CatalogJson.objects.filter(filters).values(
+                "id",
+                "catalog_name",
+                "catalog_category",
+                "catalog_category_name",
+                "catalog_subcategory_name",
+            )
+        else:
+            info = CatalogJson.objects.all().values(
+                "id",
+                "catalog_name",
+                "catalog_category",
+                "catalog_category_name",
+                "catalog_subcategory_name",
+            )
+
         res = {}
-        res['total_all'] = len(info)
-        res['dataset'] = {}
+        res["total_all"] = len(info)
+        res["dataset"] = {}
 
-        lang = request.query_params.get('lang', 'en')
-        lang_mapping = {'en' : 0, 'bm' : 1}
+        lang = request.query_params.get("lang", "en")
+        lang_mapping = {"en": 0, "bm": 1}
 
-        if lang not in lang_mapping : 
-            lang = 'en'
+        if lang not in lang_mapping:
+            lang = "en"
 
         for item in info.iterator():
-            category = item['catalog_category_name'].split(" | ")[ lang_mapping[lang] ]            
-            sub_category = item['catalog_subcategory_name'].split(" | ")[ lang_mapping[lang] ]
-            
-            obj = {}
-            obj['catalog_name'] = item['catalog_name'].split(" | ")[ lang_mapping[lang] ]
-            obj['id']= item['id']
+            category = item["catalog_category_name"].split(" | ")[lang_mapping[lang]]
+            sub_category = item["catalog_subcategory_name"].split(" | ")[
+                lang_mapping[lang]
+            ]
 
-            if category not in res['dataset'] : 
-                res['dataset'][category] = {}
-                res['dataset'][category][sub_category] = [obj]
-            else : 
-                if sub_category in res['dataset'][category] : 
-                    res['dataset'][category][sub_category].append(obj)
-                else : 
-                    res['dataset'][category][sub_category] = [obj]
+            obj = {}
+            obj["catalog_name"] = item["catalog_name"].split(" | ")[lang_mapping[lang]]
+            obj["id"] = item["id"]
+
+            if category not in res["dataset"]:
+                res["dataset"][category] = {}
+                res["dataset"][category][sub_category] = [obj]
+            else:
+                if sub_category in res["dataset"][category]:
+                    res["dataset"][category][sub_category].append(obj)
+                else:
+                    res["dataset"][category][sub_category] = [obj]
 
         return JsonResponse(res, safe=False)
 
 
-'''
+"""
 Checks which filters have been applied for the data-catalog
-'''
-def get_filters_applied(param_list) : 
-    default_params = {'period' : '', 'geographic' : [], 'begin' : '', 'end' : '', 'source' : [], 'search' : ''}
+"""
 
-    for k, v in default_params.items() : 
-        if k in param_list : 
-            if isinstance(v , str) : # Else get the multi-support
+
+def get_filters_applied(param_list):
+    default_params = {
+        "period": "",
+        "geographic": [],
+        "begin": "",
+        "end": "",
+        "source": [],
+        "search": "",
+    }
+
+    for k, v in default_params.items():
+        if k in param_list:
+            if isinstance(v, str):  # Else get the multi-support
                 default_params[k] = param_list[k][0]
-            else : 
-                default_params[k] = param_list[k][0].split(',')
-    
-    # Check if period, geographic, source has been set
-    if default_params['period'] == '' : 
-        default_params.pop('period', None)
-    if default_params['geographic'] == [] : 
-        default_params.pop('geographic', None)
-    if default_params['source'] == [] : 
-        default_params.pop('source', None)
-    if default_params['search'] == '' :
-        default_params.pop('search', None)
-    if default_params['begin'] == '' : 
-        default_params.pop('begin', None)
-    if default_params['end'] == '' : 
-        default_params.pop('end', None)        
+            else:
+                default_params[k] = param_list[k][0].split(",")
+
+    # Check if parameters have been set, remove those which have not
+    default_params = {k: v for k, v in default_params.items() if v}
 
     query = Q()
 
-    for k, v in default_params.items() : 
-        if k == 'period' : 
+    for k, v in default_params.items():
+        if k == "period":
             query &= Q(time_range=v)
-        elif k == 'geographic' :         
-            for i in v :
+        elif k == "geographic":
+            for i in v:
                 query |= Q(geographic__contains=i)
-        elif k == 'dataset_range' : 
+        elif k == "dataset_range":
             query &= Q(dataset_range__contains=v)
-        elif k == 'source' : 
+        elif k == "source":
             query &= Q(data_source__in=tuple(v))
-        elif k == 'search' : 
+        elif k == "search":
             query &= Q(catalog_name__icontains=v)
-        if k == 'begin' : 
+        if k == "begin":
             query &= Q(dataset_begin__lte=v)
-        if k == 'end' : 
+        if k == "end":
             query &= Q(dataset_end__gte=v)
-    
+
     return query
 
-'''
+
+"""
 Handles the data-variable queries, by chart applied
-'''
-def data_variable_chart_handler(data, chart_type, param_list) : 
-    if chart_type == 'TIMESERIES' :
-        filter = ''
-        range = ''
-        
-        for i in data['API']['filters'] : 
-            if i['key'] == 'filter' : 
-                filter = i['default']['value']
-            elif i['key'] == 'range' : 
-                range = i['default']['value']
+"""
 
-        if 'filter' in param_list : 
-            filter = param_list['filter'][0]
 
-        if 'range' in param_list : 
-            range =  param_list['range'][0]
+def data_variable_chart_handler(data, chart_type, param_list):
+    if chart_type == "TIMESERIES":
+        filter = ""
+        range = ""
+
+        for i in data["API"]["filters"]:
+            if i["key"] == "filter":
+                filter = i["default"]["value"]
+            elif i["key"] == "range":
+                range = i["default"]["value"]
+
+        if "filter" in param_list:
+            filter = param_list["filter"][0]
+
+        if "range" in param_list:
+            range = param_list["range"][0]
 
         table_data = {}
 
-        if filter :
-            table_data['columns'] = data['chart_details']['chart'][filter]['TABLE']['columns'] 
-            table_data['data'] = data['chart_details']['chart'][filter]['TABLE']['data'][range]
-            chart_data = data['chart_details']['chart'][filter][range]
-            intro = data['chart_details']['intro']
-        else :
-            table_data['columns'] = data['chart_details']['chart']['TABLE']['columns'] 
-            table_data['data'] = data['chart_details']['chart']['TABLE']['data'][range]
-            chart_data = data['chart_details']['chart'][range]
-            intro = data['chart_details']['intro']       
+        if filter:
+            table_data["columns"] = data["chart_details"]["chart"][filter]["TABLE"][
+                "columns"
+            ]
+            table_data["data"] = data["chart_details"]["chart"][filter]["TABLE"][
+                "data"
+            ][range]
+            chart_data = data["chart_details"]["chart"][filter][range]
+            intro = data["chart_details"]["intro"]
+        else:
+            table_data["columns"] = data["chart_details"]["chart"]["TABLE"]["columns"]
+            table_data["data"] = data["chart_details"]["chart"]["TABLE"]["data"][range]
+            chart_data = data["chart_details"]["chart"][range]
+            intro = data["chart_details"]["intro"]
 
-        return {'chart_data' : chart_data, 'table_data' : table_data, 'intro' : intro}
-    elif chart_type == 'CHOROPLETH' : 
-        if 'filters' not in data['API'] :
+        return {"chart_data": chart_data, "table_data": table_data, "intro": intro}
+    elif chart_type == "CHOROPLETH":
+        if "filters" not in data["API"]:
             table_data = {}
-            table_data['columns'] = data['chart_details']['chart']['TABLE']['columns'] 
-            table_data['data'] = data['chart_details']['chart']['TABLE']['data']
-            chart_data = data['chart_details']['chart']['CHART']
-            intro = data['chart_details']['intro']
-            return {'chart_data' : chart_data, 'table_data' : table_data, 'intro' : intro}
-    elif chart_type == 'TABLE' :
-        intro = data['chart_details']['intro']
-        return {'table_data' : data['chart_details']['chart'], 'intro' : intro}
+            table_data["columns"] = data["chart_details"]["chart"]["TABLE"]["columns"]
+            table_data["data"] = data["chart_details"]["chart"]["TABLE"]["data"]
+            chart_data = data["chart_details"]["chart"]["CHART"]
+            intro = data["chart_details"]["intro"]
+            return {"chart_data": chart_data, "table_data": table_data, "intro": intro}
+    elif chart_type == "TABLE":
+        intro = data["chart_details"]["intro"]
+        return {"table_data": data["chart_details"]["chart"], "intro": intro}
 
-'''
+
+"""
 General handler for data-variables
-'''
-def data_variable_handler(param_list) :
+"""
+
+
+def data_variable_handler(param_list):
     var_id = param_list["id"][0]
-    info = CatalogJson.objects.filter(id=var_id).values('catalog_data')
-    info = info[0]['catalog_data']
-    chart_type = info['API']['chart_type']
+    info = CatalogJson.objects.filter(id=var_id).values("catalog_data")
+    info = info[0]["catalog_data"]
+    chart_type = info["API"]["chart_type"]
 
-    info['chart_details'] = data_variable_chart_handler(info, chart_type, param_list)
+    info["chart_details"] = data_variable_chart_handler(info, chart_type, param_list)
 
-    if len(info) == 0 : 
+    if len(info) == 0:
         return {}
     return info
 
-'''
+
+"""
 Handles request for dashboards
-'''
+"""
+
+
 def handle_request(param_list):
     dbd_name = str(param_list["dashboard"][0])
     dbd_info = cache.get("META_" + dbd_name)
 
-    if not dbd_info :
-        dbd_info = MetaJson.objects.filter(dashboard_name=dbd_name).values('dashboard_meta')
+    if not dbd_info:
+        dbd_info = MetaJson.objects.filter(dashboard_name=dbd_name).values(
+            "dashboard_meta"
+        )
 
     params_req = []
 
     if len(dbd_info) > 0:
-        dbd_info = dbd_info if isinstance(dbd_info, dict) else dbd_info[0]["dashboard_meta"]
+        dbd_info = (
+            dbd_info if isinstance(dbd_info, dict) else dbd_info[0]["dashboard_meta"]
+        )
         params_req = dbd_info["required_params"]
 
     res = {}
     if all(p in param_list for p in params_req):
-        data = dbd_info['charts']
-        
-        if len(data) > 0 :
-            for k, v in data.items() :
-                api_type = v['api_type']
-                api_params = v['api_params']
+        data = dbd_info["charts"]
+
+        if len(data) > 0:
+            for k, v in data.items():
+                api_type = v["api_type"]
+                api_params = v["api_params"]
                 cur_chart_data = cache.get(dbd_name + "_" + k)
 
-                if not cur_chart_data :
-                    cur_chart_data = DashboardJson.objects.filter(dashboard_name=dbd_name, chart_name=k).values('chart_data')[0]['chart_data']                    
+                if not cur_chart_data:
+                    cur_chart_data = DashboardJson.objects.filter(
+                        dashboard_name=dbd_name, chart_name=k
+                    ).values("chart_data")[0]["chart_data"]
                     cache.set(dbd_name + "_" + k, cur_chart_data)
 
-                data_as_of = None if 'data_as_of' not in cur_chart_data else cur_chart_data['data_as_of']
+                data_as_of = (
+                    None
+                    if "data_as_of" not in cur_chart_data
+                    else cur_chart_data["data_as_of"]
+                )
 
-                if api_type == 'static' :
+                if api_type == "static":
                     res[k] = {}
-                    if data_as_of : 
-                        res[k]['data_as_of'] = data_as_of 
-                    res[k]['data'] = cur_chart_data['data']
-                else : 
+                    if data_as_of:
+                        res[k]["data_as_of"] = data_as_of
+                    res[k]["data"] = cur_chart_data["data"]
+                else:
                     if len(api_params) > 0:
-                        cur_chart_data = get_nested_data(api_params, param_list, cur_chart_data['data'])
-                    
+                        cur_chart_data = get_nested_data(
+                            api_params, param_list, cur_chart_data["data"]
+                        )
+
                     if len(cur_chart_data) > 0:
                         res[k] = {}
-                        if data_as_of : 
-                            res[k]['data_as_of'] = data_as_of                      
-                        res[k]['data'] = cur_chart_data
+                        if data_as_of:
+                            res[k]["data_as_of"] = data_as_of
+                        res[k]["data"] = cur_chart_data
     return res
 
-'''
+
+"""
 Slices dictionary,
 based on keys within dictionary
-'''
+"""
 
-def get_nested_data(api_params, param_list, data) :
+
+def get_nested_data(api_params, param_list, data):
     for a in api_params:
         if a in param_list:
-            key = param_list[a][0] if "__FIXED__" not in a else a.replace("__FIXED__", "")
+            key = (
+                param_list[a][0] if "__FIXED__" not in a else a.replace("__FIXED__", "")
+            )
             if key in data:
                 data = data[key]
         else:
             data = {}
             break
-    
+
     return data
 
-'''
+
+"""
 Checks whether or not,
 a request made is valid
-'''
+"""
 
-def is_valid_request(request, workflow_token) : 
+
+def is_valid_request(request, workflow_token):
     if "Authorization" not in request.headers:
         return False
-        
+
     secret = request.headers.get("Authorization")
     if secret != workflow_token:
         return False
 
-    return True    
+    return True
